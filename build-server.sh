b8d5b1c888dd519dabc35736d378a5f781dab1ed
Oleg Ivanov <oivanov@rn-leasing.ru>
2024-10-02 12:33:04 +0000

    edit build-server.sh again

--- build-server.sh
+++ build-server.sh
@@ -2,4 +2,3 @@
 cd ./server_new/
 docker build -t $DOCKER_REGISTRY_URL/onec-server:$ONEC_VERSION .
 docker push $DOCKER_REGISTRY_URL/onec-server:$ONEC_VERSION
-docker push $DOCKER_REGISTRY_URL/onec-server:$ONEC_VERSION
